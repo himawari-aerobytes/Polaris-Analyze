f86a484e6ebb600682a5f1e050277bedaed1ab5e
n-syuichi <syuichi.nakajima.8t@stu.hosei.ac.jp>
2021-11-04 15:22:36 +0900

    Fix Add Analyze

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,100 +1,79 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Counter {
-    private final List<String> CATEGORIES = new ArrayList<>(Arrays.asList("ゼミ入退室連絡", "ゼミ緊急", "GR連絡", "一般", "資料作成連絡", "GR緊急", "共通連絡", "共通緊急"));
-    private int***REMOVED******REMOVED*** receive = new int***REMOVED***CATEGORIES.size()***REMOVED***;
-    private int***REMOVED******REMOVED*** read = new int***REMOVED***CATEGORIES.size()***REMOVED***;
-    private int index = 0;
-    public final int MaxIndex = CATEGORIES.size();
+    private final String***REMOVED******REMOVED*** CATEGORIES = {"ゼミ入退室連絡", "ゼミ緊急", "GR連絡", "一般", "資料作成連絡", "GR緊急", "共通連絡", "共通緊急"};
+    private Map<String,Integer> receive = new HashMap<String,Integer>();
+    private Map<String,Integer> read = new HashMap<String,Integer>();
 
-    public int classNameToIndex(String className){
-        return CATEGORIES.indexOf(className);
+    public String***REMOVED******REMOVED*** getCATEGORIES() {
+        return CATEGORIES;
   ***REMOVED***
 
-    public int getMaxIndex(){
-        return this.MaxIndex;
-  ***REMOVED***
-
-    public int getAllReceived(){
-        int allReceive = 0;
-
-        for(int receive : this.receive){
-            allReceive+=receive;
+    public Counter(){
+        System.out.println("Counter--Constructor");
+        for(String CATEGORY : CATEGORIES){
+            this.receive.put(CATEGORY,0);
       ***REMOVED***
 
-        return allReceive;
+        for(String CATEGORY : CATEGORIES){
+            this.read.put(CATEGORY,0);
+      ***REMOVED***
+  ***REMOVED***
 
+
+    public int getAllReceived(){
+        int allReceived=0;
+        for(String CATEGORY : CATEGORIES){
+            allReceived += this.receive.get(CATEGORY);
+      ***REMOVED***
+        return allReceived;
   ***REMOVED***
 
     public int getAllRead(){
-        int allRead = 0;
-        for(int read : this.read){
-            allRead += read;
+        int allRead=0;
+        for(String CATEGORY : CATEGORIES){
+            allRead += this.read.get(CATEGORY);
       ***REMOVED***
         return allRead;
-
-  ***REMOVED***
-    public void Counter(){
-        this.index = 0;
-        this.receive = new int***REMOVED***CATEGORIES.size()***REMOVED***;
-        this.read = new int***REMOVED***CATEGORIES.size()***REMOVED***;
   ***REMOVED***
 
-    public int getIndex(){
-        return this.index;
+    public void addReceive(String messageType)
+  ***REMOVED***
+        int receive = this.receive.get(messageType);
+        this.receive.replace(messageType,++receive);
+
   ***REMOVED***
 
-    public void setIndex(int index){
-        if(index < 0 && CATEGORIES.size() <= index ){
-            throw new ArrayIndexOutOfBoundsException("indexがcategoriesの配列の範囲を指定していません．");
-      ***REMOVED***
-        this.index = index;
+    public void addRead(String messageType){
+        int read = this.receive.get(messageType);
+        this.read.replace(messageType,++read);
   ***REMOVED***
 
-    public void setIndex(String className){
-        this.index = CATEGORIES.indexOf(className);
-  ***REMOVED***
+    public boolean canCalcPercentage(String messageType){
 
-    public void addReceive(){
-        this.receive***REMOVED***this.index***REMOVED***++;
-  ***REMOVED***
-
-    public void addRead(){
-        this.read***REMOVED***this.index***REMOVED***++;
-  ***REMOVED***
-
-    public boolean canCalcPercentage(){
-        if(this.receive***REMOVED***this.index***REMOVED*** != 0){
-            return true;
-      ***REMOVED***
-        return false;
-  ***REMOVED***
-    private boolean canCalcPercentage(int index){
-        if(this.receive***REMOVED***index***REMOVED*** != 0){
+        if(this.receive.get(messageType) != 0){
             return true;
       ***REMOVED***
         return false;
   ***REMOVED***
 
-    public double calcPercentage(){
-        if(!canCalcPercentage()){
-            throw new ArithmeticException(CATEGORIES.get(this.index)+"の受信は１つもありませんでした");
+    public double calcPercentage(String messageType){
+        if(!canCalcPercentage(messageType)){
+            throw new ArithmeticException(messageType + "の受信は１つもありませんでした");
       ***REMOVED***
-        return (double) this.read***REMOVED***this.index***REMOVED*** / this.receive***REMOVED***this.index***REMOVED***;
+
+        return (double) this.read.get(messageType) / this.receive.get(messageType);
   ***REMOVED***
 
     public double calcAllPercentage(){
-        int i;
         int receive = 0;
         int read = 0;
 
-        for(i=0;i<CATEGORIES.size();i++){
-            receive += this.receive***REMOVED***i***REMOVED***;
-            read += this.read***REMOVED***i***REMOVED***;
+        for(String CATEGORY : CATEGORIES){
+            read += this.read.get(CATEGORY);
+            receive += this.receive.get(CATEGORY);
       ***REMOVED***
-
 
         if(0==receive){
             return -1;
