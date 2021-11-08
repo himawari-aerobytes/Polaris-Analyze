a3a18a195c8abe57a8c247347f33e8a3a7c1e51f
n-syuichi <syuichi.nakajima.8t@stu.hosei.ac.jp>
2021-11-08 15:34:52 +0900

    Refactor CSVProcessor.java

--- src/main/java/CSVProcessor.java
+++ src/main/java/CSVProcessor.java
@@ -1,10 +1,10 @@
 
 
+
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
-import org.mozilla.universalchardet.UniversalDetector;
 import java.io.*;
 import java.rmi.server.ExportException;
 import java.util.List;
@@ -13,30 +13,20 @@ import java.util.List;
  * CSVをJSONに変換します
  */
 public class CSVProcessor {
-    public static String ReadCSV(String _path) throws IOException{
+    public static String ReadCSV(String path) throws IOException{
         final ObjectMapper objectMapper = new ObjectMapper();
         final CsvMapper mapper = new CsvMapper();
 
-        // ヘッダあり
         CsvSchema schema = mapper.schemaFor(Message.class).withHeader();
-        File file = new File(_path);
-
-        FileInputStream input = new FileInputStream(file);
+        FileInputStream input = new FileInputStream(path);
 
         //shift-jis to utf-8
-        InputStreamReader stream = new InputStreamReader(input,"SJIS");
+        InputStreamReader stream = new InputStreamReader(input,"Shift-JIS");
 
         try (BufferedReader br =  new BufferedReader(stream);) {
-
             MappingIterator<Message> it = mapper.readerFor(Message.class).with(schema).readValues(br);
-
             // CSVファイルを全行まとめて読み込む場合
             List<Message> messageList = it.readAll();
-
-            for(Message message : messageList){
-                System.out.println(message.getSender());
-
-            }
 
             return objectMapper.writeValueAsString(messageList);
 
