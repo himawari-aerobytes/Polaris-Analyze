7be85d27f344e3a558c7fe86201adb5449bc1bd5
n-syuichi <syuichi.nakajima.8t@stu.hosei.ac.jp>
2021-11-08 18:51:09 +0900

    Refactor CSVProcessor.java

--- src/main/java/CSVDeserializer.java
+++ src/main/java/CSVDeserializer.java
@@ -1,6 +1,3 @@
-
-
-
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
@@ -12,20 +9,21 @@ import java.util.List;
 /**
  * CSVをJSONに変換します
  */
-public class CSVProcessor {
+public class CSVDeserializer {
     public static String ReadCSV(String path) throws IOException{
         final ObjectMapper objectMapper = new ObjectMapper();
         final CsvMapper mapper = new CsvMapper();
 
         CsvSchema schema = mapper.schemaFor(Message.class).withHeader();
-        FileInputStream input = new FileInputStream(path);
 
+        FileInputStream input = new FileInputStream(path);
         //shift-jis to utf-8
         InputStreamReader stream = new InputStreamReader(input,"Shift-JIS");
 
-        try (BufferedReader br =  new BufferedReader(stream);) {
+
+        try (BufferedReader br =  new BufferedReader(stream)) {
+
             MappingIterator<Message> it = mapper.readerFor(Message.class).with(schema).readValues(br);
-            // CSVファイルを全行まとめて読み込む場合
             List<Message> messageList = it.readAll();
 
             return objectMapper.writeValueAsString(messageList);
