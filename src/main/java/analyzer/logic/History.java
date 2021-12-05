720c82e4a2d0e4f7b47c2dc82782f09c594acc8b
n-syuichi <syuichi.nakajima.8t@stu.hosei.ac.jp>
2021-12-05 22:22:35 +0900

    Delete Lab Class

--- src/main/java/analyzer/logic/History.java
+++ src/main/java/analyzer/logic/History.java
@@ -1,4 +1,4 @@
-package analyzer.analyze.logic;
+package analyzer.logic;
 
 import analyzer.io.CSVDeserializer;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -8,7 +8,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import static java.lang.System.exit;
 
@@ -16,17 +15,17 @@ public class History {
     final private ObjectMapper mapper = new ObjectMapper();
     private List<Message> History;
     private FileInputStream MembersFile;
-    private List<Map<String, String>> MembersJSON;
     private List<Member> members= new ArrayList<>();
-    private Lab lab;
+    //private Lab lab;
 
     public History(){}
 
     public History(String filename) {
+        FileInputStream MembersFile = null;
 
         try{
             this.History = CSVDeserializer.ReadCSV(filename);
-            this.MembersFile = new FileInputStream("Members.json");
+            MembersFile = new FileInputStream("Members.json");
             this.members = mapper.readValue(MembersFile, new TypeReference<List<Member>>() {
             });
         }catch (IOException e){
@@ -34,8 +33,8 @@ public class History {
            exit(-1);
         }
 
-        lab = new Lab(this.members);
-        System.out.println(lab.getMembers().get(0));
+        //lab = new Lab(this.members);
+        //System.out.println(lab.getMembers().get(0));
 
     }
 
@@ -43,13 +42,9 @@ public class History {
         return History;
     }
 
-    public List<Map<String, String>> getMembersJSON(){
-        return MembersJSON;
-    }
-
-    public Lab getLab() {
-        return lab;
-    }
+    //public Lab getLab() {
+    //    return lab;
+    //}
     public List<Member> getMembers(){
         return members;
     }
